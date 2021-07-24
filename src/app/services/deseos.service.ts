5b7f8d41bb0641bfb0c3d123077fdd91b677ce53
Jose PR <josepr1987@gmail.com>
2021-07-24 11:37:13 +0200

    Creando servicios, modelos de datos creados

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ListaItem } from '../models/lista-item.model';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista[] = [];
+
+  constructor() {
+
+    const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('Héroes a desaparecer');
+
+    this.listas.push(lista1, lista2);
+
+  }
+
+}
+
